6e19b110c1e78d158830fa87682f9624d971e00c
Mike Fulton <mikefultonpersonal@gmail.com>
2021-11-12 17:23:52 -0600

    base code

---
+++ perlbuild.sh
@@ -0,0 +1,78 @@
+#!/bin/sh 
+#
+# Pre-requisites: 
+#  - cd to the directory of this script before running the script   
+#  - ensure you have sourced setenv.sh, e.g. . ./setenv.sh
+#  - ensure you have GNU make installed (4.1 or later)
+#  - ensure you have access to c99
+#  - network connectivity to pull git source from org
+#
+if [ "${PERL_ROOT}" = '' ]; then
+	echo "Need to set PERL_ROOT - source setenv.sh" >&2
+	exit 16
+fi
+if [ "${PERL_VRM}" = '' ]; then
+	echo "Need to set PERL_VRM - source setenv.sh" >&2
+	exit 16
+fi
+
+make --version >/dev/null 2>&1 
+if [ $? -gt 0 ]; then
+	echo "You need GNU Make on your PATH in order to build PERL" >&2
+	exit 16
+fi
+
+whence c99 >/dev/null
+if [ $? -gt 0 ]; then
+	echo "c99 required to build PERL. " >&2
+	exit 16
+fi
+
+git clone https://github.com/Perl/perl5.git --branch "${PERL_VRM}" --single-branch --depth 1 
+
+cd Perl
+MY_ROOT="${PWD}"
+
+chtag -R -h -t -cISO8859-1 "${MY_ROOT}"
+if [ $? -gt 0 ]; then
+	echo "Unable to tag PERL directory tree as ASCII" >&2
+	exit 16
+fi
+
+#
+# Apply patches
+#
+if [ "${PERL_VRM}" = "maint-5.34" ]; then
+	echo "add patches here"
+fi
+
+#
+# Setup the configuration 
+#
+sh Configure -de
+if [ $? -gt 0 ]; then
+	echo "Configure of PERL tree failed." >&2
+	exit 16
+fi
+
+make
+if [ $? -gt 0 ]; then
+	echo "MAKE of PERL tree failed." >&2
+	exit 16
+fi
+
+
+cd "${DELTA_ROOT}/tests"
+export PATH="${PERL_ROOT}/${PERL_VRM}/src:${PATH}"
+
+./runbasic.sh
+if [ $? -gt 0 ]; then
+	echo "Basic test of PERL failed." >&2
+	exit 16
+fi
+./runexamples.sh
+if [ $? -gt 0 ]; then
+	echo "Example tests of PERL failed." >&2
+	exit 16
+fi
+exit 0
---
+++ setenv.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#set -x
+
+if ! [ -f ./setenv.sh ]; then
+	echo "Need to source from the setenv.sh directory" >&2
+else
+	export _BPXK_AUTOCVT="ON"
+	export _CEE_RUNOPTS="FILETAG(AUTOCVT,AUTOTAG),POSIX(ON),TERMTHDACT(MSG)"
+	export _TAG_REDIR_ERR="txt"
+	export _TAG_REDIR_IN="txt"
+	export _TAG_REDIR_OUT="txt"
+
+	export M4_VRM="maint-5.34"
+	export M4_ROOT="${PWD}"
+fi
