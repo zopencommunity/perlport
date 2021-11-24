5f1b74c9525ba7059499cb6ff17e1db2b00b78fb
Mike Fulton <mikefultonpersonal@gmail.com>
2021-11-24 13:55:11 -0600

    clean up patches so not reversed and enhance patch management to make it easy to create new patches

---
+++ patches/managepatches.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+#
+# Manage patches
+# To create a new patch:
+#   -cd to patches directory, create any sub-directories if required, and create an empty patch file by touch'ing it
+# To refresh patches:
+#   -run managepatches
+#   -This will look at all the patch files, update them, and then patch the source files
+#
+set -x
+if [ $# -ne 0 ]; then
+	echo "Syntax: managepatches" >&2
+	echo "  refreshes patch files" >&2
+	exit 8
+fi
+
+PERLPORT_ROOT="$1"
+mydir="$(dirname $0)"
+PERLPORT_ROOT=`cd $mydir/..; echo $PWD`
+
+if [ "${PERL_ROOT}" = '' ]; then
+	echo "Need to set PERL_ROOT - source setenv.sh" >&2
+	exit 16
+fi
+if [ "${PERL_VRM}" = '' ]; then
+	echo "Need to set PERL_VRM - source setenv.sh" >&2
+        exit 16
+fi
+
+if [ "${PERL_VRM}" = "maint-5.34" ]; then
+	CODE_ROOT="${PERLPORT_ROOT}/perl5"
+	PATCH_ROOT="${PERLPORT_ROOT}/patches"
+	patches=`cd ${PATCH_ROOT}; find . -name "*.patch"`
+	for patch in $patches; do
+		rp="${patch%*.patch}"
+		o="${CODE_ROOT}/${rp}.orig"
+		f="${CODE_ROOT}/${rp}"
+		p="${PATCH_ROOT}/${rp}.patch"
+
+		if [ -f "${o}" ]; then
+			# Original file exists. Regenerate patch, then replace file with original version 
+			diff -C 2 -f "${o}" "${f}" >"${p}"
+			cp "${o}" "${f}"
+		else
+			# Original file does not exist yet. Create original file
+			cp "${f}" "${o}"
+		fi
+
+		patch -c "${f}" <"${p}"
+		if [ $? -gt 0 ]; then
+			echo "Patch of perl tree failed (${f})." >&2
+			exit 16
+		fi
+	done
+fi
+
+exit 0	
--- perlbuild.sh
+++ perlbuild.sh
@@ -33,53 +33,20 @@ if ! [ -d perl5 ]; then
 	git clone https://github.com/Perl/perl5.git --branch "${PERL_VRM}" --single-branch --depth 1 
 fi
 
-MY_ROOT="${PWD}"
-
-cd perl5
-chtag -R -h -t -cISO8859-1 "${MY_ROOT}/perl5"
+PERLPORT_ROOT="${PWD}"
+chtag -R -h -t -cISO8859-1 "${PERLPORT_ROOT}/perl5"
 if [ $? -gt 0 ]; then
 	echo "Unable to tag PERL directory tree as ASCII" >&2
 	exit 16
 fi
 
-#
-# Apply patches
-# To create a new patch:
-# cd to perl5 directory
-# copy original file in perl5 directory to: <file>.orig
-# diff -C 2 -f <file>.c <file>.orig >../patches/<file>.patch  
-#
-if [ "${PERL_VRM}" = "maint-5.34" ]; then
-	# Copy files to 'orig' version if not already copied
-	# otherwise restore so that this step can be repeated
-	if ! [ -f doio.c.orig ]; then
-		cp doio.c doio.c.orig
-	else
-		
-	fi
+./patches/applypatches.sh "${PERLPORT_ROOT}"
+rc=$?
+if [ $rc -gt 0 ]; then
+	exit $rc
+fi
 
-	patch -c doio.c <${MY_ROOT}/patches/doio.patch
-	if [ $? -gt 0 ]; then
-  		echo "Patch of perl tree failed (doio.c)." >&2
-                exit 16
-	fi      
-	patch -c iperlsys.h <${MY_ROOT}/patches/iperlsys.patch
-  	if [ $? -gt 0 ]; then
-                echo "Patch of perl tree failed (iperlsys.h)." >&2
-                exit 16
-        fi      
-	patch -R -c hints/os390.sh <${MY_ROOT}/patches/os390.patch
-  	if [ $? -gt 0 ]; then
-                echo "Patch of perl tree failed (hints/os390.sh)." >&2
-                exit 16
-        fi      
-	patch -c cpan/Perl-OSType/lib/Perl/OSType.pm <${MY_ROOT}/patches/OSType.patch
-  	if [ $? -gt 0 ]; then
-                echo "Patch of perl tree failed (cpan/Perl-OSType/lib/Perl/OSType.pm)." >&2
-                exit 16
-        fi      
-fi  
-
+cd perl5
 #
 # Setup the configuration 
 #
