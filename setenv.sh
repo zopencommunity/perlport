a225cd7ecdc1d3c314c59cd9cd20ec2cefb48f77
Mike Fulton <mikefultonpersonal@gmail.com>
2021-11-26 14:04:32 -0600

    restrict LIBPATH and PATH to ensure no 'old' perl used for the build

--- setenv.sh
+++ setenv.sh
@@ -10,6 +10,13 @@ else
 	export _TAG_REDIR_IN="txt"
 	export _TAG_REDIR_OUT="txt"
 
+	if [ "$HOME" != '' ] && [ -f $HOME/bin ]; then
+		export PATH=$HOME/bin:/usr/local/bin:/bin:/usr/sbin
+	else
+		export PATH=/usr/local/bin:/bin:/usr/sbin
+	fi  
+	export LIBPATH=/lib:/usr/lib
+
 	export PERL_VRM="maint-5.34"
 	export PERL_ROOT="${PWD}"
 fi
