766cfff7e149d18bf6133bc6186a98b31100043d
Mike Fulton <mikefultonpersonal@gmail.com>
2021-12-02 14:32:59 -0600

    add in ascii support - should be nop under ebcdic

--- setenv.sh
+++ setenv.sh
@@ -18,10 +18,10 @@ else
 	export LIBPATH=/lib:/usr/lib
 
 	# See perlbuild.sh for valid values of PERL_xxx variables
-	export PERL_VRM="blead"
-	export PERL_OS390_TGT_AMODE="64" 
-	export PERL_OS390_TGT_LINK="dynamic" 
-	export PERL_OS390_TGT_CODEPAGE="ascii" 
+	export PERL_VRM="maint-5.34" # blead
+	export PERL_OS390_TGT_AMODE="31" # 64 
+	export PERL_OS390_TGT_LINK="static" # dynamic
+	export PERL_OS390_TGT_CODEPAGE="ebcdic" # ascii
 
 	export PERL_ROOT="${PWD}"
 	export GIT_ROOT=/rsusr/ported/bin
