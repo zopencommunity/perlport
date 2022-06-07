6c8bbeac8268f1d449b0465453f7346c909bbca5
Igor Todorovski <itodorov@ca.ibm.com>
2022-06-06 21:22:13 -0400

    Add portcrtenv

--- portcrtenv.sh
+++ portcrtenv.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-install_dir="$1"
+INSTALL_DIR="$1"
 
-cat <<zz >"${install_dir}/.env"
-if ! [ -f ./.env ]; then
-  echo "Need to source from the .env directory" >&2
-  return 0
+cat <<EOF > ${INSTALL_DIR}/.env
+export _BPXK_AUTOCVT=ON
+export _CEE_RUNOPTS="\$_CEE_RUNOPTS FILETAG(AUTOCVT,AUTOTAG) POSIX(ON)"
+export _TAG_REDIR_IN=txt
+export _TAG_REDIR_ERR=txt
+export _TAG_REDIR_OUT=txt
+export PATH="${INSTALL_DIR}/bin:\$PATH"
+for libperl in \$(find ${INSTALL_DIR} -name "libperl.so"); do
+  libperl=\$(dirname "\${libperl}")
+  export LIBPATH="\${libperl}:\$LIBPATH"
+done
+if ls ${INSTALL_DIR}/lib/perl[0-9]* >/dev/null 2>&1; then
+  PERL5LIB_ROOT=\$( cd ${INSTALL_DIR}/lib/perl[0-9]/[0-9]*; echo \$PWD )
+else
+  PERL5LIB_ROOT=\$( cd ${INSTALL_DIR}/lib/[0-9]*; echo \$PWD )
 fi
-mydir="\${PWD}"
-export PATH="\${mydir}/bin:\$PATH"
-zz
+export PERL5LIB="\${PERL5LIB_ROOT}:\${PERL5LIB_ROOT}/os390"
+EOF
+
 exit 0
--- setenv.sh
+++ setenv.sh
@@ -9,26 +9,11 @@ fi
 
 export PORT_ROOT="${PWD}"
 export PORT_TYPE="GIT"
-
-PORT_GIT_DIR=perl
-
-if [ ! -z "${PERL_INSTALL_DIR}" ]; then
-  install_dir=${PERL_INSTALL_DIR}
-else
-  install_dir="${HOME}/local/perl"
-fi
-
-mkdir -p $install_dir
-if [ $? -gt 0 ]; then
-  echo "Install directory $install_dir cannot be created"
-  exit 16
-fi
-
 export PORT_CONFIGURE="./Configure"
 export PORT_CONFIGURE_OPTS="-Dprefix=$install_dir -Duserelocatableinc -Dusedevel -des -Duse64bitall -Dusedl"
 export PORT_CHECK_OPTS="test"
 export PORT_GIT_URL="https://github.com/Perl/perl5.git"
-#export PORT_GIT_DEPS="git make"
+export PORT_GIT_DEPS="git make"
 export PORT_GIT_BRANCH="blead"
 
 # Perl Environment variables
