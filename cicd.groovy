4b816b870929303baf442381c954f56e0aa5e46b
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:03 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'perlport'), string(name: 'DESCRIPTION', 'perlport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'perlport'), string(name: 'DESCRIPTION', 'Perl is a family of two high-level, general-purpose, interpreted, dynamic programming languages.' )]
         }
 }
