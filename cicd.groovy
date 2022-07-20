995abfc07ac0b59df94cb16ac5af07ffd28f203e
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:36 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'perlport'), string(name: 'DESCRIPTION', 'Perl is a family of two high-level, general-purpose, interpreted, dynamic programming languages.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'perlport'), string(name: 'DESCRIPTION', value: 'Perl is a family of two high-level, general-purpose, interpreted, dynamic programming languages.' )]
         }
 }
