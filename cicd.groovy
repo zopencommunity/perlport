66fd6f5e24c63b57dfecb9e926914c4a069db802
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:19 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/perlport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/perlport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/perlport.git'), string(name: 'PORT_DESCRIPTION', value: 'Perl is a family of two high-level, general-purpose, interpreted, dynamic programming languages.' ), string(name: 'NODE_LABEL', value: "v2r4")]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/perlport.git'), string(name: 'PORT_DESCRIPTION', value: 'Perl is a family of two high-level, general-purpose, interpreted, dynamic programming languages.' ), string(name: 'NODE_LABEL', value: "v2r4")]
         }
 }
